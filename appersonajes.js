502e6da296bbe415e25c9c96c7e6b08e47787cf6
jtsujtsu <jjjsjjjs@gmail.com>
2023-12-23 19:49:25 -0300

    nueva actualizacion

--- appersonajes.js
+++ appersonajes.js
@@ -1,61 +1,100 @@
-let pagina = 3;
+let pagina = 7;
 
 const btnuno = document.getElementById("btnuno");
 const btndos = document.getElementById("btndos");
 
 const $contenedor = document.getElementById("persona");
-const cargarpersonajes = `https://rickandmortyapi.com/api/character/?page=${pagina}`;
+const carperso = `https://rickandmortyapi.com/api/character/?page=${pagina}`;
 
 btnuno.addEventListener("click", () => {
   pagina = 1;
-  cargarpersonajes();
+
+  const carperso = `https://rickandmortyapi.com/api/character/?page=${pagina}`;
+  console.log(carperso);
+  console.log(pagina);
+  console.log(fetch(carperso));
+
+  fetch(carperso)
+    .then((response) => {
+      return response.json();
+    })
+
+    .then((datoperso) => {
+      const persona = datoperso.results;
+      // persona.length
+      for (let i = 0; i < persona.length; i++) {
+        //desctructuring
+        const { name, gender, species, status, image } = persona[i];
+        console.log(persona[i]);
+      }
+    });
 });
 
 btndos.addEventListener("click", () => {
-  pagina = 5;
-  cargarpersonajes();
+  pagina = 2;
+
+  const carperso = `https://rickandmortyapi.com/api/character/?page=${pagina}`;
+  console.log(carperso);
+  console.log(pagina);
+  console.log(fetch(carperso));
+
+  fetch(carperso)
+    .then((response) => {
+      return response.json();
+    })
+
+    .then((datoperso) => {
+      const persona = datoperso.results;
+      // persona.length
+      for (let i = 0; i < persona.length; i++) {
+        //desctructuring
+        const { name, gender, species, status, image } = persona[i];
+        console.log(persona[i]);
+      }
+    });
 });
+
+console.log(carperso);
+console.log(pagina);
+console.log(fetch(carperso));
 
 // API
 // Contenedor de html para renderizar contenido
-// promesas (asincronnismo en JS
+// promesas asincronnismo en
 
-fetch(cargarpersonajes)
+fetch(carperso)
   .then((response) => {
     return response.json();
   })
 
-  .then((data) => {
-    const persona = data.results;
-
+  .then((datoperso) => {
+    const persona = datoperso.results;
     // persona.length
     for (let i = 0; i < persona.length; i++) {
       //desctructuring
       const { name, gender, species, status, image } = persona[i];
-
+      console.log(persona[i]);
       $contenedor.innerHTML += `
-      
-      <div class="persona-control">
-      <div class="texto-total">
-      <h5>${pagina}</h5>
-       <img
-        class="ima-contor"
-        src="${image}"
-        alt="imagen de ${name}"
-       />
-       <div class="texto-control">
-        <div class="tex-control">
-         <h5>${name}</h5>         
-          <b>
-           <h6>Estado:${status}</h6>
-           <h6>Género:${gender}</h6>
-           <h6>Especie:${species}</h6>
-          </b>
+       <div class="persona">
+       <div class="texto-total">
+       <h5>${pagina}</h5>
+        <img
+         class="ima-contor"
+         src="${image}"
+         alt="imagen de ${name}"
+        />
+        <div class="texto-control">
+         <div class="tex-control">
+          <h5>${name}</h5>         
+           <b>
+            <h6>Estado:${status}</h6>
+            <h6>Género:${gender}</h6>
+            <h6>Especie:${species}</h6>
+           </b>
+         </div>
         </div>
        </div>
-      </div>
-      </div>
-  
+      </div>  
       `;
     }
   });
